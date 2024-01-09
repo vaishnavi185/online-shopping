991bd36157bb06ebc556fa33a392761269e42cc9
vaishnavi185 <vishu032004@gmail.com>
2024-01-09 23:20:34 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './home.css'
+
+export default function Home() {
+  return (
+    
+    <div>
+      <h3>Welcome</h3>
+      <h1>TRY SOMETHING AMAZING</h1>
+      <h2>Ordering your fave Pizza is quick and easy with our</h2>
+      <h2> app or on our website.</h2>
+      <div className='row'>
+         <button className='b3'>Read Blog</button>
+         <button className='b4'>View</button>
+
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'; // Import the shopping cart icon
+import './nav.css';
+
+export default function Navbar() {
+  return (
+    <div className='header'>
+      <li>
+        <img src="/nav.png" alt="image" />
+      </li>
+      <li>
+        <img src="/nav2.png" alt="image" />
+      </li>
+      <div className='divs'>
+        <li><a href='#html'>Home </a></li>
+        <li><a href='#html'>Menu </a></li>
+        <li><a href='#html'>Blog</a></li>
+        <li><a href='#html'>About </a></li>
+        <li><a href='#html'>Contact</a></li>
+        <li><button className='b1'>Log in</button></li>
+        <li>
+          <button className='b2'>
+            <i className="fas fa-shopping-cart"></i>
+            <FontAwesomeIcon icon={faShoppingCart} /> Cart (0)
+          </button>
+        </li>
+      </div>
+    </div>
+  );
+}
